c2dd88fe9299f2654769823534125c5011eb9025
Hytufan <58300067+Hytufan@users.noreply.github.com>
2026-06-16 22:52:27 +0800

    Add files via upload

---
+++ pysgg/engine/inference.py
@@ -0,0 +1,295 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+import logging
+import os
+
+import json
+import torch
+from tqdm import tqdm
+import psutil
+import pynvml
+
+from pysgg.config import cfg
+from pysgg.data.datasets.evaluation import evaluate
+from .bbox_aug import im_detect_bbox_aug
+from ..utils.comm import all_gather
+from ..utils.comm import is_main_process, get_world_size
+from ..utils.comm import synchronize
+from ..utils.timer import Timer, get_time_str
+
+from torchvision.transforms.functional import to_pil_image
+
+def get_gpu_stats():
+    """Get GPU usage stats using pynvml."""
+    pynvml.nvmlInit()
+    handle = pynvml.nvmlDeviceGetHandleByIndex(3)
+    gpu_util = pynvml.nvmlDeviceGetUtilizationRates(handle).gpu
+    memory_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    gpu_mem_used = memory_info.used / 1024**2
+    gpu_mem_total = memory_info.total / 1024**2
+    pynvml.nvmlShutdown()
+    return gpu_util, gpu_mem_used, gpu_mem_total
+
+def get_cpu_stats():
+    """Get CPU and memory stats for the current Python process."""
+    process = psutil.Process(os.getpid())
+    cpu_usage = process.cpu_percent(interval=1)  # 获取当前进程的 CPU 使用率
+    memory_info = process.memory_info()
+    memory_used = memory_info.rss / 1024**2  # 以MB为单位的内存使用量
+    return cpu_usage, memory_used
+
+
+
+def compute_on_dataset(model, data_loader, device, synchronize_gather=True, timer=None, logger=None,cam_extractor=None):
+    """
+
+    :param model:
+    :param data_loader:
+    :param device:
+    :param synchronize_gather:  gather the predictions during the training,
+                                rather than gathering all predictions after the training
+    :param timer:
+    :return:
+    """
+    if cam_extractor is None:
+        model.eval()
+        results_dict = {}
+        cpu_device = torch.device("cpu")
+        #img_obj_info={}
+        gpu_util, gpu_mem_used, gpu_mem_total = get_gpu_stats()
+        cpu_util, cpu_mem_used = get_cpu_stats()
+        logger.info(f"GPU Utilization: {gpu_util}% | GPU Memory Used: {gpu_mem_used:.2f} MiB/{gpu_mem_total:.2f} MiB")
+        logger.info(f"CPU Utilization: {cpu_util}% | CPU Memory Used: {cpu_mem_used:.2f} MiB")
+        
+        for _, batch in enumerate(tqdm(data_loader)):
+        #for i, batch in enumerate(data_loader):
+            #print(i,len(data_loader),end='\r')
+            with torch.no_grad():
+                images, targets, protect_targets = batch
+                image_ids = [protect_target.get_field("index") for protect_target in protect_targets]
+                #print(data_loader.dataset.filenames[image_ids[0]])
+                for protect_target in protect_targets:
+                    protect_target.remove_field("index")
+                if targets[0] == None:
+                    targets=None
+                else:
+                    targets = [target.to(device) for target in targets]
+                protect_targets = [protect_target.to(device) for protect_target in protect_targets]
+                if timer:
+                    timer.tic()
+                if cfg.TEST.BBOX_AUG.ENABLED:
+                    output = im_detect_bbox_aug(model, images, device)
+                else:
+                    # relation detection needs the targets
+                    output = model(images.to(device), targets, protect_targets, logger=logger)
+                if timer:
+                    if not cfg.MODEL.DEVICE == 'cpu':
+                        torch.cuda.synchronize()
+                    timer.toc()
+                output = [o.to(cpu_device) for o in output]
+            if synchronize_gather:
+                synchronize()
+                multi_gpu_predictions = all_gather({img_id: result for img_id, result in zip(image_ids, output)})
+                if is_main_process():
+                    for p in multi_gpu_predictions:
+                        results_dict.update(p)
+            else:
+                results_dict.update(
+                    {img_id: result for img_id, result in zip(image_ids, output)}
+                )
+                
+        gpu_util, gpu_mem_used, gpu_mem_total = get_gpu_stats()
+        cpu_util, cpu_mem_used = get_cpu_stats()
+        logger.info(f"GPU Utilization: {gpu_util}% | GPU Memory Used: {gpu_mem_used:.2f} MiB/{gpu_mem_total:.2f} MiB")
+        logger.info(f"CPU Utilization: {cpu_util}% | CPU Memory Used: {cpu_mem_used:.2f} MiB")
+        
+        
+        return results_dict
+    else:
+        model.eval()
+        results_dict = {}
+        active_map_list=[]
+        cpu_device = torch.device("cpu")
+        #img_obj_info={}
+        for _, batch in enumerate(tqdm(data_loader)):
+            #with torch.no_grad():
+            images, targets, protect_targets = batch
+            image_ids = [protect_target.get_field("index") for protect_target in protect_targets]
+            for protect_target in protect_targets:
+                protect_target.remove_field("index")
+            if targets[0] == None:
+                targets=None
+            else:
+                targets = [target.to(device) for target in targets]
+            protect_targets = [protect_target.to(device) for protect_target in protect_targets]
+            if timer:
+                timer.tic()
+            if cfg.TEST.BBOX_AUG.ENABLED:
+                output = im_detect_bbox_aug(model, images, device)
+            else:
+                # relation detection needs the targets
+                output = model(images.to(device), targets, protect_targets, logger=logger)
+            if timer:
+                if not cfg.MODEL.DEVICE == 'cpu':
+                    torch.cuda.synchronize()
+                timer.toc()
+            output = [o.to(cpu_device) for o in output]
+            '''for res in output:
+                active_map=cam_extractor(class_idx=res.get_field("pred_event_label").item(), scores=res.get_field("el"))
+                active_map=[a.to(cpu_device) for a in active_map]
+                active_map=to_pil_image(active_map[0].squeeze(0), mode='F')
+                active_map_list.append(active_map)'''
+            if synchronize_gather:
+                synchronize()
+                multi_gpu_predictions = all_gather({img_id: result for img_id, result in zip(image_ids, output)})
+                if is_main_process():
+                    for p in multi_gpu_predictions:
+                        results_dict.update(p)
+            else:
+                results_dict.update(
+                    {img_id: result for img_id, result in zip(image_ids, output)}
+                )
+            #torch.cuda.empty_cache()
+        
+        return results_dict #,active_map_list
+
+
+def _accumulate_predictions_from_multiple_gpus(predictions_per_gpu, synchronize_gather=True):
+    if not synchronize_gather:
+        all_predictions = all_gather(predictions_per_gpu)
+    if not is_main_process():
+        return
+
+    if synchronize_gather:
+        predictions = predictions_per_gpu
+    else:
+        # merge the list of dicts
+        predictions = {}
+        for p in all_predictions:
+            predictions.update(p)
+
+    # convert a dict where the key is the index in a list
+    image_ids = list(sorted(predictions.keys()))
+    if len(image_ids) != image_ids[-1] + 1:
+        
+        logger = logging.getLogger("pysgg.inference")
+        logger.warning(
+            "WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!"
+            "Number of images that were gathered from multiple processes is not "
+            "a contiguous set. Some images might be missing from the evaluation"
+        )
+        logger.info(f"len(image_ids) {len(image_ids)},  image_ids[-1] + 1 {image_ids[-1] + 1}")
+
+    # convert to a list
+    predictions = [predictions[i] for i in image_ids]
+    return predictions
+
+
+def inference(
+        cfg,
+        model,
+        data_loader,
+        dataset_name,
+        iou_types=("bbox",),
+        box_only=False,
+        device="cuda",
+        expected_results=(),
+        expected_results_sigma_tol=4,
+        output_folder=None,
+        logger=None,
+        cam_extractor=None,
+):
+    load_prediction_from_cache = cfg.TEST.ALLOW_LOAD_FROM_CACHE and output_folder is not None and os.path.exists(
+        os.path.join(output_folder, "eval_results.pytorch"))
+    # convert to a torch.device for efficiency
+    device = torch.device(device)
+    num_devices = get_world_size()
+    if logger is None:
+        logger = logging.getLogger("pysgg.inference")
+    dataset = data_loader.dataset
+    logger.info("Start evaluation on {} dataset({} images).".format(dataset_name, len(dataset)))
+    total_timer = Timer()
+    inference_timer = Timer()
+    total_timer.tic()
+    active_map_list=None
+    if load_prediction_from_cache:
+        logging.info("load_prediction_from_cache: " + os.path.join(output_folder, "eval_results.pytorch"))
+        predictions = torch.load(os.path.join(output_folder, "eval_results.pytorch"),
+                                 map_location=torch.device("cpu"))['predictions']
+    else:
+        if cam_extractor is None:
+            predictions = compute_on_dataset(model, data_loader, device,
+                                            synchronize_gather=cfg.TEST.RELATION.SYNC_GATHER,
+                                            timer=inference_timer, logger=logger,
+                                            cam_extractor=cam_extractor)
+        else:
+            predictions = compute_on_dataset(model, data_loader, device,  #predictions,active_map_list
+                                            synchronize_gather=cfg.TEST.RELATION.SYNC_GATHER,
+                                            timer=inference_timer, logger=logger,
+                                            cam_extractor=cam_extractor)
+    # wait for all processes to complete before measuring the time
+    synchronize()
+    total_time = total_timer.toc()
+    total_time_str = get_time_str(total_time)
+    logger.info(
+        "Total run time: {} ({} s / img per device, on {} devices)".format(
+            total_time_str, total_time * num_devices / len(dataset), num_devices
+        )
+    )
+    total_infer_time = get_time_str(inference_timer.total_time)
+    logger.info(
+        "Model inference time: {} ({} s / img per device, on {} devices)".format(
+            total_infer_time,
+            inference_timer.total_time * num_devices / len(dataset),
+            num_devices,
+        )
+    )
+
+    if not load_prediction_from_cache:
+        predictions = _accumulate_predictions_from_multiple_gpus(predictions,
+                                                                 synchronize_gather=cfg.TEST.RELATION.SYNC_GATHER)
+
+    if not is_main_process():
+        return -1.0
+
+    '''print(f'prediction:{predictions}')
+    if output_folder is not None and not load_prediction_from_cache:
+        torch.save(predictions, os.path.join(output_folder, "predictions.pth"))
+
+    groundtruths = []
+    for image_id, prediction in enumerate(predictions):
+        img_info = dataset.get_img_info(image_id)
+        image_width = img_info["width"]
+        image_height = img_info["height"]
+        # recover original size which is before transform
+        predictions[image_id] = prediction.resize((image_width, image_height))
+
+        gt = dataset.get_groundtruth(image_id, evaluation=True)
+        groundtruths.append(gt)
+
+    save_output(output_folder, groundtruths, predictions, dataset)'''
+
+    extra_args = dict(
+        box_only=box_only,
+        iou_types=iou_types,
+        expected_results=expected_results,
+        expected_results_sigma_tol=expected_results_sigma_tol,
+    )
+    '''if cfg.TEST.CUSTUM_EVAL:
+        detected_sgg = custom_sgg_post_precessing(predictions)
+        with open(os.path.join(cfg.DETECTED_SGG_DIR, 'custom_prediction.json'), 'w') as outfile:  
+            json.dump(detected_sgg, outfile)
+        print('=====> ' + str(os.path.join(cfg.DETECTED_SGG_DIR, 'custom_prediction.json')) + ' SAVED !')
+        return -1.0'''
+    if active_map_list is not None:
+        return evaluate(cfg=cfg,
+                    dataset=dataset,
+                    predictions=predictions,
+                    output_folder=output_folder,
+                    logger=logger,
+                    **extra_args)
+    return evaluate(cfg=cfg,
+                    dataset=dataset,
+                    predictions=predictions,
+                    output_folder=output_folder,
+                    logger=logger,
+                    **extra_args)
